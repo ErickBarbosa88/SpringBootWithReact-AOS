e9c6fca7263518cf0e964920af85970118908253
ErickBarbosa88 <barbosaerick88@gmail.com>
2023-05-06 12:42:15 -0300

    done

---
+++ src/main/java/com/example/atividadespring/OlaController.java
@@ -0,0 +1,14 @@
+package com.example.atividadespring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OlaController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "Dale " + name;
+    }
+}
---
+++ src/test/java/com/example/atividadespring/AtividadespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
